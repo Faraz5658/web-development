9babdbf235d2dc995685ffa9eb88e0a8d92008fb
Faraz5658 <farazahmad5623@gmail.com>
2025-07-22 22:30:26 +0500

    let add in js

---
+++ javascript/LET/EXAMPLE NO 1/let no 1 example.js
@@ -0,0 +1,6 @@
+//// Re assignment using 'let'
+let x = "Faraz";
+console.log('x 1: ', x);//  Faraz
+
+x="Bangash";
+console.log('x 2: ', x); //  Bangash
---
+++ javascript/LET/EXAMPLE NO 2/let no 2 example.js
@@ -0,0 +1,9 @@
+//  // Re definition using 'let'
+
+let x = "Faraz";
+console.log('x: ', x);
+
+let x = "Bangash"
+console.log('x: ', x);
+
+// Output  Error
